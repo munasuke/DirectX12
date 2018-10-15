d448bdc0a8c6e2375e51f72a0f6acc37a11f8a4d
munasuke <34709203+munasuke@users.noreply.github.com>
2018-10-15 18:47:37 +0900

    Revert "xssdhdsajl"
    This reverts commit 775c74b745c30f61fc40e795550069515c0f4f91.

---
+++ DirectX12_64/DirectX12_64/Source/ImageLoader.cpp
@@ -0,0 +1,69 @@
+#include "ImageLoader.h"
+#include <locale>
+#include <codecvt>
+
+using namespace DirectX;
+
+ImageLoader::ImageLoader() {
+	//拡張子でロード関数を分ける
+	//bmp, png, jpg用
+	loadFuncTbl[L"bmp"] = loadFuncTbl[L"png"] = loadFuncTbl[L"jpg"] =
+		[](const std::wstring& path, TexMetadata* meta, ScratchImage& img) -> HRESULT {
+		return LoadFromWICFile(path.c_str(), 0, meta, img); };
+
+	//tga用
+	loadFuncTbl[L"tga"] = 
+		[](const std::wstring& path, TexMetadata* meta, ScratchImage& img) -> HRESULT {
+		return LoadFromTGAFile(path.c_str(), meta, img); };
+
+	//dds用
+	loadFuncTbl[L"dds"] =
+		[](const std::wstring& path, TexMetadata* meta, ScratchImage& img) -> HRESULT {
+		return LoadFromDDSFile(path.c_str(), 0, meta, img); };
+}
+
+int ImageLoader::Load(const std::string path) {
+	//string → wstring
+	std::wstring wstrPath = ConvertStringToWString(path);
+	//拡張子を判定
+	std::wstring filePath = wstrPath;
+	auto index = filePath.rfind('.');
+	filePath = filePath.substr(index + 1);
+
+	auto result = loadFuncTbl[filePath](wstrPath, &metaData, image);
+	metaDataArray.emplace_back(metaData);
+	imageArray.emplace_back(image.GetPixels());
+
+	return 0;
+}
+
+DirectX::TexMetadata ImageLoader::GetMetaData()
+{
+	return metaData;
+}
+
+std::vector<DirectX::TexMetadata> ImageLoader::GetMetaArray() {
+	return metaDataArray;
+}
+
+uint8_t * ImageLoader::GetScratchImage() {
+	auto imgPixels = image.GetPixels();
+	return imgPixels;
+}
+
+std::vector<uint8_t*> ImageLoader::GetScratchImageArray() {
+	return imageArray;
+}
+
+ImageRect ImageLoader::GetImageRect() {
+	return imageRec;
+}
+
+
+ImageLoader::~ImageLoader() {
+}
+
+std::wstring ImageLoader::ConvertStringToWString(std::string str) {
+	std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> cv;
+	return cv.from_bytes(str);
+}
